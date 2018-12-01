ca5866b2d60d814b1043863546fa8839cf4a22c1
Gerry Waterston <gwaterston@hotmail.co.uk>
2018-12-01 20:26:06 +0000

    Commit song_list tests

--- room.rb
+++ room.rb
@@ -1,25 +1,29 @@
+require('pry')
 
 class Room
 
-attr_reader :name, :guest
+attr_reader :name, :guest, :song
 
   def initialize(name)
     @name = name
     @guest = []
+    @song = []
   end
 
-  def guests_in_room()
+  def guests_number()
     return @guest.length()
   end
 
   def check_in_guest(guest)
-  @guest.push(guest)
+    @guest.push(guest)
   end
 
+  def song_list_length()
+    return @song.length()
+  end
 
-
-
-  # def song_list()
-  #end
+  def song_list(song)
+    @song.push(song)
+  end
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -3,6 +3,7 @@ require('minitest/rg')
 
 require_relative('../room')
 require_relative('../guest')
+require_relative('../song')
 
 class RoomTest < MiniTest::Test
 
@@ -12,7 +13,10 @@ class RoomTest < MiniTest::Test
     # so not used for calculations
     @guest1 = Guest.new("Charlie")
     @guest2 = Guest.new("Sam")
-    # @guest[@guest1, @guest2]
+    #Guests added to array of guests
+    @song1 = Song.new({title: "Dancing Queen", artist: "ABBA"})
+    @song2 = Song.new({title: "Park Life", artist: "Blur"})
+
   end
 
   def test_room_has_a_name
@@ -20,19 +24,38 @@ class RoomTest < MiniTest::Test
   end
 #passed test
 
-  def test_check_for_number_guests_in_room
-    assert_equal(0, @room.guests_in_room())
+  def test_check_for_guests_checked_into_room
+    assert_equal(0, @room.guests_number())
   end
 #passed test
 
-  def test_check_in_guests
+  def test_guests_checked_into_room
     @room.check_in_guest(@guest1)
-    assert_equal(1, @room.guests_in_room())
+    assert_equal(1, @room.guests_number())
   end
+#passed test
 
-  def test_check_in_multiple_guests
+  def test_check_in_two_guests
     @room.check_in_guest(@guest1)
     @room.check_in_guest(@guest2)
-    assert_equal(2, @room.guests_in_room())
+    assert_equal(2, @room.guests_number())
   end
+#passed test
+
+  def test_check_for_songs_added_to_list
+    assert_equal(0, @room.song_list_length())
+  end
+#passed
+
+def test_add_song_to_song_list
+  @room.song_list(@song1)
+  assert_equal(1, @room.song_list_length())
+end
+
+def test_add_two_song_to_song_list
+  @room.song_list(@song1)
+  @room.song_list(@song2)
+  assert_equal(2, @room.song_list_length())
+end
+
 end
